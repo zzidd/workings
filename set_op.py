3b80fdff85107f0df69ad39b53564d93596e1568
zzidd <97144261+zzidd@users.noreply.github.com>
2022-01-07 21:27:15 +0530

    Create set_op.py

---
+++ set_op.py
@@ -0,0 +1,6 @@
+E = {1,2,3,4,5}
+N = {4,5,6,7,8}
+print("Union of sets:",E|N)
+print("Intersection of sets:",E&N)
+print("Difference between sets:",E-N)
+print("Symmetric difference between sets:",E^N)
